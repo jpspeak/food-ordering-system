8df820a63fef744c02f9ed55ec72f2c2e9de5aa2
jpspeak <jayphilipcamillo@gmail.com>
2020-09-25 11:54:34 +0800

    Added App.js, Index.js

--- resources/views/spa_view.blade.php
+++ resources/views/spa_view.blade.php
@@ -5,10 +5,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Food Ordering System</title>
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <!-- Scripts -->
+    <script defer src="{{asset('js/app.js')}}"></script>
 </head>
 
-<body>
-    <div id='app'></div>
+<body style="margin: 0; padding: 0">
+    <div id="app"></div>
 </body>
 
 </html>
